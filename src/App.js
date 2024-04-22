572f44912101d54e216c347e8e296bc9ec52e853
ssooya <ssooya90@naver.com>
2024-04-22 09:26:34 +0900

    리엑트 함수형 컴포넌트 rsc 프로퍼티 (읽기전용) 공부

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import TodaysPlan from './03/TodaysPlan.jsx';
+
+function App() {
+
+  return (
+    <TodaysPlan/>
+  );
+}
+
+export default App;
